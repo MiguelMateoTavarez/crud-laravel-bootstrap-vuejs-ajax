fc6fee5b36088559cf931fed57e34e55c1385874
MiguelMateoTavarez <miguelmateot94@gmail.com>
2021-02-07 19:34:18 -0400

    CRUD controller created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use Symfony\Component\Routing\Matcher\RedirectableUrlMatcher;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
         return view('create');
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'profession' => 'required',
+        ]);
+
+        User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'profession' => $request->input('profession'),
+        ]);
+
+        return redirect('/');
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'profession' => 'required',
+        ]);
+
+        $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'profession' => $request->input('profession'),
+        ]);
+
+        return redirect('/');
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        $user->delete();
+
+        return redirect('/');
     }
 }
